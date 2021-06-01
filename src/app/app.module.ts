123d0c8c32898418e6b6bf09d87444d91488ac1e
svasquez <svasquez@controldiseno.com>
2021-06-01 10:46:23 -0500

    Fin maquetacion proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NoPageFountComponent } from './pages/no-page-fount/no-page-fount.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BreadCrumbsComponent } from './shared/bread-crumbs/bread-crumbs.component';
+import { SideBarComponent } from './shared/side-bar/side-bar.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NoPageFountComponent,
+    DashboardComponent,
+    BreadCrumbsComponent,
+    SideBarComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProgressComponent,
+    Grafica1Component,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/no-page-fount/no-page-fount.component.ts
@@ -0,0 +1,20 @@
+import { GlobalService } from './../../services/global.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-no-page-fount',
+  templateUrl: './no-page-fount.component.html',
+  styleUrls: ['./no-pages-found.component.css'
+  ]
+})
+export class NoPageFountComponent implements OnInit {
+
+  year: number;
+  constructor(private global:GlobalService) {
+    this.year = this.global.myDate.getFullYear();
+  }
+
+  ngOnInit(): void {
+  }
+
+}
